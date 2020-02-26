2f4d90a2cacc9e3b68712d1524e0255e844a7707
bsteyer <brian.steyer@gmail.com>
2020-02-26 03:49:55 +0000

    Connected to JavaScript Document

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function)() {
+
+
+var levels = {
+  "map": [[0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0]],
+  "counts": [0,3,1,0,0,1],
+  "starts": [2,0]
+  "startDir": "R"
+  "endSide":"right"
+  }
+}
+var currentLvl = 1;
+var levelName = "level"+currentLvl;
+});
+
+
+  
