4e710d74cd2caf8995eab6a6e7db4c5dd0abb002
bsteyer <brian.steyer@gmail.com>
2020-02-26 04:04:25 +0000

    Updated Script with new JavaScript code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function)() {
 
-
-var levels = {
+var levels = new Array {
   "map": [[0,0,0,0],
           [0,0,0,0],
           [0,0,0,0],
@@ -15,6 +14,47 @@ var levels = {
 var currentLvl = 1;
 var levelName = "level"+currentLvl;
 });
+
+  initialize();
+
+ function initialize(){
+  $("#title").html("Connect the Pipes");
+  // initialize the drop zone's size and position
+  $("#drop-zone").empty();
+  var width = levels[levelName].map[0].length * 4;
+  var height = levels[levelName].map.length * 4;
+ $("#drop-zone").css({
+ "width": width+"rem",
+ "height": height+"rem",
+ "margin-left": (24-width)/2+"rem",
+ "margin-top": (24-height)/2+"rem"
+ });
+ // create the grid
+ for(var i = 0; i < levels[levelName].map.length;i++){
+for(var j = 0; j < levels[levelName].map[i].length; j++){
+ var id = i+"_"+j
+ $("#drop-zone").append("<div id ='"+id+"'"+
+ "class='pipe'>"+
+ "<div class='p"+
+levels[levelName].map[i][j]+
+ "'></div></div>");
+ }
+ }
+ // create the start wall
+  var r = levels[levelName].start[0];
+  var c = levels[levelName].start[1];
+  $("#"+r+"_"+c).css("border-"+levels[levelName].startSide+"-color",
+"blue");
+  // create the end wall
+  r = levels[levelName].end[0];
+  c = levels[levelName].end[1];
+  $("#"+r+"_"+c).css("border-"+levels[levelName].endSide+"-color",
+"red");
+ }
+});
+
+
+
 
 
   
